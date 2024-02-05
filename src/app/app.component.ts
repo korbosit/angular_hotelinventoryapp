0bc7177cec97f3f99d810e7a2ab3c80f6d8a2865
Vlaimir Korba <krissky1986@gmail.com>
2024-02-05 14:45:16 +0200

    components, template, styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,16 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // template: `<h1>Hello world from inline template</h1>
+  //   <p>Angular is Awesome</p> `,
+  styleUrl: './app.component.scss',
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: red;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
