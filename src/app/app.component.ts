56934d98144427580f136de21458ec0ec2351f86
Vlaimir Korba <krissky1986@gmail.com>
2024-02-08 16:10:52 +0200

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomsComponent } from './rooms/rooms.component';
+// import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'hinv-root',
   standalone: true,
-  imports: [RouterOutlet, RoomsComponent],
+  // imports: [RouterOutlet, RoomsComponent, NgSwitch, NgSwitchCase],
+  imports: [RouterOutlet, RoomsComponent, CommonModule],
   templateUrl: './app.component.html',
   // template: `<h1>Hello world from inline template</h1>
   //   <p>Angular is Awesome</p> `,
@@ -20,4 +23,5 @@ import { RoomsComponent } from './rooms/rooms.component';
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+  role = 'Admin';
 }
